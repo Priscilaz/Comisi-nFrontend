0c52f431ad8db7c3fac8ae0a01da576f7878702e
Priscilaz <priscila.zuniga@udla.edu.ec>
2025-06-22 21:29:44 -0500

    Cambio en la llamada al backend

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,4 @@
-const API_BASE = 'https://comisi-nbackend.onrender.com/api/comisiones';
+const API_BASE = 'https://comisi-nbackend.onrender.com';
 
 async function fetchJSON(url, opts) {
   const res = await fetch(url, opts);
